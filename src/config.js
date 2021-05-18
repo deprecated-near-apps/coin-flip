99d68b1a116149ed5939de0395f80e4cbcb6d47f
Josh <thisisjoshford@gmail.com>
2021-05-17 18:41:59 -0700

    update contract name and cred path;

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = "dev-1612932767425-7586350";
+const contractName = 'dev-1621302059600-80550158376073';
 
 module.exports = function getConfig(isServer = false) {
   let config = {
@@ -6,7 +6,7 @@ module.exports = function getConfig(isServer = false) {
     nodeUrl: "https://rpc.testnet.near.org",
     walletUrl: "https://wallet.testnet.near.org",
     helperUrl: "https://helper.testnet.near.org",
-    CONTRACT_NAME,
+    contractName,
   };
 
   if (process.env.REACT_APP_ENV !== undefined) {
@@ -28,7 +28,7 @@ module.exports = function getConfig(isServer = false) {
       nodeUrl: "https://rpc.mainnet.near.org",
       walletUrl: "https://wallet.near.org",
       helperUrl: "https://helper.mainnet.near.org",
-      CONTRACT_NAME: "near",
+      contractName: "near",
     };
   }
 
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,36 +1,59 @@
-const BN = require('bn.js');
-const fetch = require('node-fetch');
-const nearAPI = require('near-api-js');
-const { KeyPair, Account, Contract, utils: { format: { parseNearAmount } } } = nearAPI;
-const { near, connection, keyStore, contract, contractAccount } = require('./near-utils');
-const getConfig = require('../src/config');
+const BN = require("bn.js");
+const fetch = require("node-fetch");
+const nearAPI = require("near-api-js");
 const {
-	networkId, contractName, contractMethods, DEFAULT_NEW_ACCOUNT_AMOUNT
-} = getConfig();
+  KeyPair,
+  Account,
+  Contract,
+  utils: {
+    format: { parseNearAmount },
+  },
+} = nearAPI;
+const {
+  near,
+  connection,
+  keyStore,
+  contract,
+  contractAccount,
+} = require("./near-utils");
+const getConfig = require("../src/config");
+const { networkId, contractName, contractMethods, DEFAULT_NEW_ACCOUNT_AMOUNT } =
+  getConfig();
 
 /********************************
 Internal Helpers
 ********************************/
-async function createAccount(accountId, fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT) {
-	const contractAccount = new Account(connection, contractName);
-	const newKeyPair = KeyPair.fromRandom('ed25519');
-	await contractAccount.createAccount(accountId, newKeyPair.publicKey, new BN(parseNearAmount(fundingAmount)));
-	keyStore.setKey(networkId, accountId, newKeyPair);
-	return new nearAPI.Account(connection, accountId);
+async function createAccount(
+  accountId,
+  fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT
+) {
+  const contractAccount = new Account(connection, contractName);
+  const newKeyPair = KeyPair.fromRandom("ed25519");
+  await contractAccount.createAccount(
+    accountId,
+    newKeyPair.publicKey,
+    new BN(parseNearAmount(fundingAmount))
+  );
+  keyStore.setKey(networkId, accountId, newKeyPair);
+  return new nearAPI.Account(connection, accountId);
 }
 
 const getSignature = async (account) => {
-	const { accountId } = account;
-	const block = await account.connection.provider.block({ finality: 'final' });
-	const blockNumber = block.header.height.toString();
-	const signer = account.inMemorySigner || account.connection.signer;
-	const signed = await signer.signMessage(Buffer.from(blockNumber), accountId, networkId);
-	const blockNumberSignature = Buffer.from(signed.signature).toString('base64');
-	return { blockNumber, blockNumberSignature };
+  const { accountId } = account;
+  const block = await account.connection.provider.block({ finality: "final" });
+  const blockNumber = block.header.height.toString();
+  const signer = account.inMemorySigner || account.connection.signer;
+  const signed = await signer.signMessage(
+    Buffer.from(blockNumber),
+    accountId,
+    networkId
+  );
+  const blockNumberSignature = Buffer.from(signed.signature).toString("base64");
+  return { blockNumber, blockNumberSignature };
 };
 
 function generateUniqueString(prefix) {
-	return `${prefix}-${Date.now()}-${Math.round(Math.random() * 1000000)}`;
+  return `${prefix}-${Date.now()}-${Math.round(Math.random() * 1000000)}`;
 }
 
 /********************************
@@ -38,78 +61,84 @@ Exports
 ********************************/
 
 async function initContract() {
-	try {
-		await contract.new({ owner_id: contractName });
-	} catch (e) {
-		if (!/Already initialized/.test(e.toString())) {
-			throw e;
-		}
-	}
-	return { contract, contractName };
+  try {
+    await contract.new({ owner_id: contractName });
+  } catch (e) {
+    if (!/Already initialized/.test(e.toString())) {
+      throw e;
+    }
+  }
+  return { contract, contractName };
 }
 
 async function getContract(account) {
-	return new Contract(account || contractAccount, contractName, {
-		...contractMethods,
-		signer: account || undefined
-	});
+  return new Contract(account || contractAccount, contractName, {
+    ...contractMethods,
+    signer: account || undefined,
+  });
 }
 
-async function getAccount(accountId, fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT) {
-	accountId = accountId || generateUniqueString('test');
-	const account = new nearAPI.Account(connection, accountId);
-	try {
-		await account.state();
-		return account;
-	} catch(e) {
-		if (!/does not exist/.test(e.toString())) {
-			throw e;
-		}
-	}
-	return await createAccount(accountId, fundingAmount);
-};
+async function getAccount(
+  accountId,
+  fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT
+) {
+  accountId = accountId || generateUniqueString("test");
+  const account = new nearAPI.Account(connection, accountId);
+  try {
+    await account.state();
+    return account;
+  } catch (e) {
+    if (!/does not exist/.test(e.toString())) {
+      throw e;
+    }
+  }
+  return await createAccount(accountId, fundingAmount);
+}
 
 const createAccessKeyAccount = (key) => {
-	connection.signer.keyStore.setKey(networkId, contractName, key);
-	return new Account(connection, contractName);
+  connection.signer.keyStore.setKey(networkId, contractName, key);
+  return new Account(connection, contractName);
 };
 
 const postSignedJson = async ({ account, contractName, url, data = {} }) => {
-	return await fetch(url, {
-		method: 'POST',
-		headers: { 'content-type': 'application/json' },
-		body: JSON.stringify({
-			...data,
-			accountId: account.accountId,
-			contractName,
-			...(await getSignature(account))
-		})
-	}).then((res) => {
-		// console.log(res)
-		return res.json();
-	});
+  return await fetch(url, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ...data,
+      accountId: account.accountId,
+      contractName,
+      ...(await getSignature(account)),
+    }),
+  }).then((res) => {
+    // console.log(res)
+    return res.json();
+  });
 };
 
 const postJson = async ({ url, data = {} }) => {
-	return await fetch(url, {
-		method: 'POST',
-		headers: { 'content-type': 'application/json' },
-		body: JSON.stringify({ ...data })
-	}).then((res) => {
-		console.log(res);
-		return res.json();
-	});
+  return await fetch(url, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ ...data }),
+  }).then((res) => {
+    console.log(res);
+    return res.json();
+  });
 };
 
-module.exports = { 
-	near,
-	connection,
-	keyStore,
-	getContract,
-	contract,
-	contractName,
-	contractMethods,
-	contractAccount,
-	createAccessKeyAccount,
-	initContract, getAccount, postSignedJson, postJson
+module.exports = {
+  near,
+  connection,
+  keyStore,
+  getContract,
+  contract,
+  contractName,
+  contractMethods,
+  contractAccount,
+  createAccessKeyAccount,
+  initContract,
+  getAccount,
+  postSignedJson,
+  postJson,
 };
